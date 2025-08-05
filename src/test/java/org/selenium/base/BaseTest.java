9ea3578792add032a96a8a5a0ddb4491f9a79e29
sonali <sonaliagrawal4@gmail.com>
2025-08-05 14:56:33 +0530

    "Added hi message in BaseTest.java"

--- src/test/java/org/selenium/base/BaseTest.java
+++ src/test/java/org/selenium/base/BaseTest.java
@@ -46,6 +46,7 @@ protected WebDriver getDriver()
 
         setDriver(new DriverManager().initializeDriver(browser));
         System.out.println("CURRENT THREAD"+ Thread.currentThread().threadId()+","+"DRIVER = "+getDriver());
+        System.out.println("hi");
     }
 
     @AfterMethod
